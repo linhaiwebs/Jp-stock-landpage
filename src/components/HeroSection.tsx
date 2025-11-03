5442f204cfb68ec3ce0a5db9309da19bd9171d65
linhaiwebs <lhwebs@outlook.com>
2025-11-03 18:37:25 +0800

    Updated NewHome.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -23,7 +23,7 @@ export default function HeroSection({ stockCode = '----', stockName = '', onDiag
 
           <div className="relative">
             <img
-              src="/assets/header copy.png"
+              src="/assets/header.png"
               alt="AI"
               className="w-32 h-32 object-contain"
             />
